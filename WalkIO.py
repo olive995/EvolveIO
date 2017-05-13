53bea15919d0e4024deb1b62b4f159497929c6e1
Olivia Blackwood <aliengravyy@gmail.com>
2017-05-13 13:46:56 -0700

    Updated the file

--- WalkIO.py
+++ WalkIO.py
@@ -1,1 +1,11 @@
-print("Hello")
+import random
+
+class chromosomes(arms, legs, head):
+    def arms(action,confidence):
+        action = []
+        good_arms = []
+        confidence = action.append.random.shuffle(action).float
+        for i in range([0.00:1.01]):
+            while confidence > ((confidence + i.float) - confidence)
+                good_arms.append(confidence)
+                return arms.action
