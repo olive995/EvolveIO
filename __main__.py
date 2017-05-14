bb86cb6188fbe5b4be88c7668ab0b4f1c92dc191
Olivia Blackwood <aliengravyy@gmail.com>
2017-05-14 15:08:59 -0700

    Test documentation

---
+++ __main__.py
@@ -0,0 +1,15 @@
+import random
+import Tkinter
+from traits import traits
+
+class chromosomes(arms, legs, head, body):
+    def __init__(self):
+        self.arms = arms
+        self.legs = legs
+        self.head = head
+        self.body = body
+
+
+    def arms(good_arms):
+        good_arms = []
+        for i in traits.confidence:
